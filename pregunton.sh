f44895708458ff36400f17346ec9fb89444e21b0
Skybreaker <diego_ms1997@hotmail.com>
2024-04-07 11:33:25 -0600

    Preguntat por todos los paises

--- pregunton.sh
+++ pregunton.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-elpais="USA"
+elpais="$1"
 echo "Hola, yo pregunto cosas"
 echo "Cual es la poblacion de $elpais?"
-grep "Mexico" paises.txt | cut -d "," -f2
+grep "$elpais" paises.txt | cut -d "," -f2
 echo "Cual es el clima de $elpais?"
 grep "$elpais" paises.txt | cut -d "," -f3
-
 
 
