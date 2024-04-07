04b6f87edd82440df37bcf5416031a0b845aa43c
Skybreaker <diego_ms1997@hotmail.com>
2024-04-07 10:50:24 -0600

    subir primera version

---
+++ pregunton.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Hola, yo pregunto cosas"
+echo "Cual es la poblacion de México?"
+grep "Mexico" paises.txt | cut -d "," -f2
+echo "Cual es el clima de Mexico?"
+grep "Mexico" paises.txt | cut -d"," -f3
+
